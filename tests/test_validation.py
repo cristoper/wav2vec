f5e562bd5165dbd179800ae713e51fbdd7b88f79
A. Cynic <chris@mretc.net>
2017-08-24 23:48:48 -0600

    Add some automated validation tests

---
+++ tests/test_validation.py
@@ -0,0 +1,116 @@
+"""
+These tests must be run with Python 3.4+
+"""
+import unittest
+import subprocess
+
+cmd = 'wav2vec.py'
+indir = 'tests/valfiles/snd'
+outdir = 'tests/valfiles/out'
+
+test_cases = [
+    # Default SVG:
+    {
+        "infile": "noise-8.wav",
+        "flags": "-f SVG",
+        "outfile": "noise-8.svg",
+    },
+    {
+        "infile": "noise-8.aiff",
+        "flags": "-f SVG",
+        "outfile": "noise-8.svg",
+    },
+    {
+        "infile": "noise-16.wav",
+        "flags": "-f SVG",
+        "outfile": "noise-16.svg",
+    },
+    {
+        "infile": "noise-16.aiff",
+        "flags": "-f SVG",
+        "outfile": "noise-16-aiff.svg",
+    },
+    {
+        "infile": "noise-32.wav",
+        "flags": "-f SVG",
+        "outfile": "noise-32.svg",
+    },
+    {
+        "infile": "noise-32.aiff",
+        "flags": "-f SVG",
+        "outfile": "noise-32-aiff.svg",
+    },
+    # Default CSV:
+    {
+        "infile": "noise-8.wav",
+        "flags": "-f CSV",
+        "outfile": "noise-8.csv",
+    },
+    {
+        "infile": "noise-8.aiff",
+        "flags": "-f CSV",
+        "outfile": "noise-8.csv",
+    },
+    {
+        "infile": "noise-16.wav",
+        "flags": "-f CSV",
+        "outfile": "noise-16.csv",
+    },
+    {
+        "infile": "noise-16.aiff",
+        "flags": "-f CSV",
+        "outfile": "noise-16-aiff.csv",
+    },
+    {
+        "infile": "noise-32.wav",
+        "flags": "-f CSV",
+        "outfile": "noise-32.csv",
+    },
+    {
+        "infile": "noise-32.aiff",
+        "flags": "-f CSV",
+        "outfile": "noise-32-aiff.csv",
+    },
+    # long, stereo, default:
+    {
+        "infile": "test-16-stereo.wav",
+        "flags": "",
+        "outfile": "test-16-stereo.svg",
+    },
+    {
+        "infile": "test-16-stereo.aiff",
+        "flags": "",
+        "outfile": "test-16-stereo-aiff.svg",
+    },
+    {
+        "infile": "test-16-stereo.wav",
+        "flags": "-f CSV",
+        "outfile": "test-16-stereo.csv",
+    },
+    {
+        "infile": "test-16-stereo.aiff",
+        "flags": "-f CSV",
+        "outfile": "test-16-stereo-aiff.csv",
+    },
+    # long, stereo, full height short width SVG:
+    {
+        "infile": "test-16-stereo.wav",
+        "flags": "--width 250 --height 0",
+        "outfile": "test-16-stereo-100x0.svg",
+    },
+]
+
+
+class TestValidationFiles(unittest.TestCase):
+    def test_all(self):
+        #self.maxDiff = None
+        for i, test in enumerate(test_cases):
+            with self.subTest(i=i):
+                with open(outdir + '/' + test['outfile']) as f:
+                    expected = f.read()
+                test_file = indir + '/' + test['infile']
+                cmd_line = "python %s %s %s" % (cmd, test['flags'], test_file)
+                result = subprocess.check_output(cmd_line.split())
+                result = result.decode('utf-8')
+                #print(test['infile'], test['outfile'])
+                self.assertEqual(result, expected)
---
+++ tests/testformatter.py
@@ -0,0 +1,5 @@
+from wav2vec.formatter import Formatter
+import unittest
+
+# TODO: maybe write some unit tests for Formatter. For now the validation tests
+# should do.
