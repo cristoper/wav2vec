3a67f6682af6c83c59bcdb52516b51918f6f8f90
A. Cynic <chris@mretc.net>
2017-08-26 14:00:31 -0600

    Rearrange flag order

--- wav2vec/main.py
+++ wav2vec/main.py
@@ -8,13 +8,20 @@ import logging
 
 
 def main():
-    aparser = argparse.ArgumentParser(description="Convert wav files to SVG graphics",
-                                      epilog="The SVG output is sent to stdout.")
+    aparser = argparse.ArgumentParser(description=("Convert WAV and AIFF files "
+                                                   "to vector (SVG, PostScript,"
+                                                   " CVS) graphics."),
+                                      epilog="The output is sent to stdout.")
     aparser.add_argument("filename", help="The WAV file to read")
+    aparser.add_argument("--format", "-f", default="SVG", type=str,
+                         choices=formatters.keys(),
+                         help="The output format, one of: SVG, CSV, PostScript. Default is SVG.")
     aparser.add_argument("--width", default=1000,
                          type=int, help=("Maximum width of generated SVG "
                                          "(graphic will be scaled down to "
                                          "this size in px)"))
+    aparser.add_argument("--height", default=500,
+                         type=int, help="Maximum height of generated SVG (graphic will be scaled down to this size in px). Note that this scales according to the highest possible amplitude (given the sample bit depth), not the highest amplitude that actually occurs in the data.")
     aparser.add_argument("--stream", metavar="BS", default=0, type=int,
                          help=("Stream the input file size in chunks (of BS "
                                "number of frames at a time) and process/format "
@@ -26,11 +33,6 @@ def main():
                                "processing."))
     aparser.add_argument("--downtoss", default=1,
                          type=int, help="Downsize by keeping only 1 out of every N samples.", metavar="N")
-    aparser.add_argument("--height", default=500,
-                         type=int, help="Maximum height of generated SVG (graphic will be scaled down to this size in px). Note that this scales according to the highest possible amplitude (given the sample bit depth), not the highest amplitude that actually occurs in the data.")
-    aparser.add_argument("--format", "-f", default="SVG", type=str,
-                         choices=formatters.keys(),
-                         help="The output format, one of: SVG, CSV, PostScript. Default is SVG.")
     aparser.add_argument("--log", dest="loglevel",
                          choices=['DEBUG', 'INFO', 'WARNING', 'ERROR',
                                   'CRITICAL'], help="Set the logging level.",
